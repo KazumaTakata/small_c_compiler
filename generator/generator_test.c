307af0d1341c3ba49a27e1ba00b2d7a745b20ae8
Kazuma Takata <neilbuzztakata@gmail.com>
2018-11-12 08:29:27 +0900

    generator

--- generator/generator_test.c
+++ generator/generator_test.c
@@ -6,6 +6,52 @@
 #include "../token/token.h"
 #include "../parser/parser.h"
 
+void GenerateExpression(struct Expression *expression)
+{
+    if (expression->token == INT)
+    {
+        int value = expression->value;
+    }
+};
+
+void GenerateStatement(struct Statement *statement, FILE *fp)
+{
+    if (statement->token == RETURN)
+    {
+        struct Expression *expression = statement->expression;
+        fprintf(fp, "\t mov rax, %d\n", expression->value);
+        fprintf(fp, "\t leave\n");
+        fprintf(fp, "\t ret\n");
+    }
+}
+
+void GenerateFunc(struct Function *func, FILE *fp)
+{
+    char *ident = func->ident;
+    struct Statement *statement = func->body;
+
+    fprintf(fp, "_%s:\n", ident);
+    fprintf(fp, "\t push rbp\n");
+    fprintf(fp, "\t mov rbp, rsp\n");
+    fprintf(fp, "\t sub rsp, 16\n");
+
+    GenerateStatement(statement, fp);
+};
+
+void GenerateAsm(struct Program *p, FILE *fp)
+{
+    struct Function *func = p->function;
+    while (1)
+    {
+        GenerateFunc(func, fp);
+        if (func->next == NULL)
+        {
+            break;
+        }
+        func = func->next;
+    }
+}
+
 int main(void)
 {
     char input_string[] = "\
@@ -13,11 +59,20 @@ int main(void)
             return 2;\
         }";
 
-    struct Lexer *l = createLexer(input_string, 0);
+    struct Lexer *l = createLexer(input_string);
 
     struct Parser *p = createParser(l);
 
     struct Program *program = ParseProgram(p);
+
+    FILE *fp = fopen("./main.asm", "w");
+
+    fprintf(fp, "\t global _main\n");
+    fprintf(fp, "\t segment .text\n");
+
+    GenerateAsm(program, fp);
+
+    fclose(fp);
 
     return 0;
 }
