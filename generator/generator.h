90b73c01d5366d38918884dff5771249496e5803
Kazuma Takata <neilbuzztakata@gmail.com>
2018-11-12 08:44:41 +0900

    generator

---
+++ generator/generator.h
@@ -0,0 +1,64 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "../lexer/lexer.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../ast/ast.h"
+#include "../token/token.h"
+#include "../parser/parser.h"
+
+void GenerateExpression(struct Expression *expression)
+{
+    if (expression->token == INT)
+    {
+        int value = expression->value;
+    }
+};
+
+void GenerateStatement(struct Statement *statement, FILE *fp)
+{
+    if (statement->token == RETURN)
+    {
+        struct Expression *expression = statement->expression;
+        fprintf(fp, "\t mov rax, %d\n", expression->value);
+        fprintf(fp, "\t leave\n");
+        fprintf(fp, "\t ret\n");
+    }
+}
+
+void GenerateFunc(struct Function *func, FILE *fp)
+{
+    char *ident = func->ident;
+    struct Statement *statement = func->body;
+
+    fprintf(fp, "_%s:\n", ident);
+    fprintf(fp, "\t push rbp\n");
+    fprintf(fp, "\t mov rbp, rsp\n");
+    fprintf(fp, "\t sub rsp, 16\n");
+
+    GenerateStatement(statement, fp);
+};
+
+void GenerateAsm(struct Program *p)
+{
+    FILE *fp = fopen("./main.asm", "w");
+
+    fprintf(fp, "\t global _main\n");
+    fprintf(fp, "\t segment .text\n");
+    struct Function *func = p->function;
+    while (1)
+    {
+        GenerateFunc(func, fp);
+        if (func->next == NULL)
+        {
+            break;
+        }
+        func = func->next;
+    }
+
+    fclose(fp);
+};
+
+#endif
--- generator/generator_test.c
+++ generator/generator_test.c
@@ -5,52 +5,7 @@
 #include "../ast/ast.h"
 #include "../token/token.h"
 #include "../parser/parser.h"
-
-void GenerateExpression(struct Expression *expression)
-{
-    if (expression->token == INT)
-    {
-        int value = expression->value;
-    }
-};
-
-void GenerateStatement(struct Statement *statement, FILE *fp)
-{
-    if (statement->token == RETURN)
-    {
-        struct Expression *expression = statement->expression;
-        fprintf(fp, "\t mov rax, %d\n", expression->value);
-        fprintf(fp, "\t leave\n");
-        fprintf(fp, "\t ret\n");
-    }
-}
-
-void GenerateFunc(struct Function *func, FILE *fp)
-{
-    char *ident = func->ident;
-    struct Statement *statement = func->body;
-
-    fprintf(fp, "_%s:\n", ident);
-    fprintf(fp, "\t push rbp\n");
-    fprintf(fp, "\t mov rbp, rsp\n");
-    fprintf(fp, "\t sub rsp, 16\n");
-
-    GenerateStatement(statement, fp);
-};
-
-void GenerateAsm(struct Program *p, FILE *fp)
-{
-    struct Function *func = p->function;
-    while (1)
-    {
-        GenerateFunc(func, fp);
-        if (func->next == NULL)
-        {
-            break;
-        }
-        func = func->next;
-    }
-}
+#include "./generator.h"
 
 int main(void)
 {
@@ -65,14 +20,7 @@ int main(void)
 
     struct Program *program = ParseProgram(p);
 
-    FILE *fp = fopen("./main.asm", "w");
-
-    fprintf(fp, "\t global _main\n");
-    fprintf(fp, "\t segment .text\n");
-
-    GenerateAsm(program, fp);
-
-    fclose(fp);
+    GenerateAsm(program);
 
     return 0;
 }
